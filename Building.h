c6219232acfd50efd4e436e544c0db4ca015ac81
DylanRMai <68725448+DylanRMai@users.noreply.github.com>
2024-12-04 19:11:25 -0800

    Update and rename Building.cpp to Building.h

---
+++ Building.h
@@ -0,0 +1,23 @@
+#ifndef BUILDING_H
+#define BUILDING_H
+#include <string>
+#include <iostream>
+#include <stream>
+#include "LinkedList.cpp"
+
+using namespace std;
+
+class Building {
+    private:
+        string name;
+        LinkedList list;
+
+    public:
+        virtual string getName();
+        virtual LinkedList* getList();
+        virtual void setName();
+        virtual void addPokemon();
+        virtual void removePokemon();
+};
+
+#endif
