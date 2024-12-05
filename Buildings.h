f56b96e2e3dab05eff7cb3cd089d02a6c93f1998
DylanRMai <68725448+DylanRMai@users.noreply.github.com>
2024-12-04 19:34:24 -0800

    Update and rename LinkedList.h to Buildings.h

---
+++ Buildings.h
@@ -0,0 +1,35 @@
+#ifndef BUILDINGS_H
+#define BUILDINGS_H
+#include "Pokemon.h"
+#include <string>
+#include <iostream>
+#include <fstream>
+
+
+struct BUILDINGS{
+   private:
+      int listSize;
+      Pokemon* head;
+      Pokemon* tail;
+   public:
+      int size()const;
+      Pokemon* getHead()const;
+      Pokemon* getTail()const;
+      void setHead(Pokemon* head);
+      void setTail(Pokemon* tail);
+      Pokemon* deleteWord(Pokemon* pokemon);
+      Pokemon* insert_before(std::string newWord, Pokemon* knownPokemon);
+      void push_back(std::string word);
+      void push_back(Pokemon* newWordPokemon);
+      void loadFile(std::string fileName);
+      void print();
+      void printReverse();
+      Buildings();
+      Buildings(std::string word);
+      ~Buildings();
+      Buildings& operator+=(Buildings& other);
+      void mergeDicts(Buildings* listB);
+      void mergeSort();
+      void mergeSort(Buildings* topListPtr);
+};
+#endif
